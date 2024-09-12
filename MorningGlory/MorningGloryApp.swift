4bd9979c4988af06654a79e83af4ab51a7456b0f
dsungc1111 <eotjd9808@naver.com>
2024-09-12 21:42:26 +0900

    Initial Commit

---
+++ MorningGlory/MorningGloryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningGloryApp.swift
+//  MorningGlory
+//
+//  Created by 최대성 on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MorningGloryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabBar()
+        }
+    }
+}
